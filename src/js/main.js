6cb97d793520507eb295e3ad942689a7aa89dc95
Jonathan Shilale <jshilale@workwave.com>
2017-09-08 11:07:46 -0400

    messed around with different function calls and added data folder with AllCards.json file in it for reference and testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,48 @@
-import injectHtml from './lib/module-example';
+// import injectHtml from './lib/module-example';
+//
+// injectHtml('module-test', 'es6 Module Test');
 
-injectHtml('module-test', 'es6 Module Test');
+const mtg = require('mtgsdk')
+
+// mtg.card.find(3)
+// .then(result => {
+//     console.log(result.card.name) // "Black Lotus"
+// });
+//
+// mtg.set.find('AER')
+// .then(result => {
+//     console.log(result.set.name) // "Aether Revolt"
+// });
+// mtg.card.find(3).then
+//   ((result => {
+//     console.log(result.card) // prints whole object
+//   }));
+
+// BELOW prints every card's imageUrl
+// mtg.card.all()
+// .on('data', function (card) {
+//   if (card.hasOwnProperty("imageUrl")) {
+//     console.log(card.imageUrl)
+//   }
+// });
+
+// BELOW searches for card name but takes a really long time to loop through all cards
+// mtg.card.all()
+// .on('data', function (card){
+//   if (card.name == "Tarmogoyf"){
+//     console.log(card);
+//   }
+//   });
+
+// ****** Suggestion, Maybe search through all the cards and only return the first result that contains an image.
+// ****** Search in multiple directions for quicker results.
+let func = 0;
+console.time(func);
+mtg.card.all()
+  .on('data', function(card){
+  if (card.name == "test"){
+    console.log(card.name);
+  }
+});
+console.timeEnd(func);
+//*** new sets "not found" query api again
