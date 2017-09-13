99ea5a44e1d48eee5ec2c119f41cc96d172e35bb
Jonathan Shilale <jshilale@workwave.com>
2017-09-13 18:18:07 -0400

    wrote file system functionality in update-data.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,47 +2,9 @@
 //
 // injectHtml('module-test', 'es6 Module Test');
 
-const mtg = require('mtgsdk')
+const mtg = require('mtgsdk');
 
-// mtg.card.find(3)
-// .then(result => {
-//     console.log(result.card.name) // "Black Lotus"
-// });
-//
-// mtg.set.find('AER')
-// .then(result => {
-//     console.log(result.set.name) // "Aether Revolt"
-// });
-// mtg.card.find(3).then
-//   ((result => {
-//     console.log(result.card) // prints whole object
-//   }));
-
-// BELOW prints every card's imageUrl
-// mtg.card.all()
-// .on('data', function (card) {
-//   if (card.hasOwnProperty("imageUrl")) {
-//     console.log(card.imageUrl)
-//   }
-// });
-
-// BELOW searches for card name but takes a really long time to loop through all cards
-// mtg.card.all()
-// .on('data', function (card){
-//   if (card.name == "Tarmogoyf"){
-//     console.log(card);
-//   }
-//   });
-
-// ****** Suggestion, Maybe search through all the cards and only return the first result that contains an image.
-// ****** Search in multiple directions for quicker results.
-let func = 0;
-console.time(func);
-mtg.card.all()
-  .on('data', function(card){
-  if (card.name == "test"){
-    console.log(card.name);
-  }
+mtg.card.find(150)
+.then(result => {
+    console.log(result.card) // "Black Lotus"
 });
-console.timeEnd(func);
-//*** new sets "not found" query api again
